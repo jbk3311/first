c41ca3f3b9591f811ae4f345357f077ec056567a
jbk3311 <jbk3311@163.com>
2015-12-10 18:42:14 +0800

    测试

---
+++ test.php
@@ -0,0 +1,18 @@
+<?php
+//生成一个连接
+$dbhost='localhost';
+$username='root';
+$userpass='szbiker';
+$dbdatabase='test';
+$db_connect=mysql_connect($dbhost,$username,$userpass) or die("Unable to connect to the MySQL!");
+
+//选择一个需要操作的数据库
+mysql_select_db($dbdatabase,$db_connect);
+
+//执行MySQL语句
+$result=mysql_query("SELECT id,name FROM student");
+
+//提取数据
+$row=mysql_fetch_row($result);
+print_r($row);
+?>
